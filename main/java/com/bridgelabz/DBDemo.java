c5a7562484b6be384969d792bf7367e9f3deda5e
vishangi <vishangid@gmail.com>
2022-07-23 11:50:10 +0530

    Modified : Ability to create a payroll service database and connect to database

--- main/java/com/bridgelabz/DBDemo.java
+++ main/java/com/bridgelabz/DBDemo.java
@@ -1,38 +1,27 @@
 package com.bridgelabz;
 
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
-import java.util.Enumeration;
+import java.sql.SQLException;
 
 public class DBDemo {
-    
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws SQLException {
         String jdbcURL = "jdbc:mysql://localhost:3307/payroll_service?useSSL=false";
         String userName = "root";
         String passWord = "1234";
-        Connection connection;
+        Connection connection = null;
         try {
-            Class.forName("com.mysql.jdbc.D river");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Driver loaded!");
-        } catch (ClassNotFoundException exception) {
-            throw new IllegalStateException("Cannot find the driver in the classpath", exception);
-        }
-        listDrivers();
-        try {
             System.out.println("Connecting to database :" + jdbcURL);
             connection = DriverManager.getConnection(jdbcURL, userName, passWord);
             System.out.println("Connection is successful!!!!" + connection);
         } catch (Exception exception) {
-            exception.printStackTrace();
-        }
-    }
-
-    private static void listDrivers() {
-        Enumeration<Driver> driverList = DriverManager.getDrivers();
-        while ((driverList.hasMoreElements())) {
-            Driver driverClass = (Driver) driverList.nextElement();
-            System.out.println(" " + driverClass.getClass().getName());
+            System.out.println(exception.getMessage());
+            ;
+        } finally {
+            connection.close();
         }
     }
 }
