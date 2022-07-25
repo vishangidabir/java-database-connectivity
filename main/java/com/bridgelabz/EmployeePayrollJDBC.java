1764fe2803114f42223fcac74f136102ae9772c2
vishangi <vishangid@gmail.com>
2022-07-25 17:08:21 +0530

    Added : Ability to add a new Employee to the Payroll

--- main/java/com/bridgelabz/EmployeePayrollJDBC.java
+++ main/java/com/bridgelabz/EmployeePayrollJDBC.java
@@ -5,42 +5,27 @@ import java.util.Scanner;
 
 public class EmployeePayrollJDBC {
 
-    public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("~~~~~Welcome to Employee Payroll program on JDBC~~~~~");
-
-        System.out.println("\nChoose option \n1 : Sum \n2 : Average \n3 : Minimum \n4 : Maximum \n5 : Count ");
-        int choice = scanner.nextInt();
-        switch (choice) {
-            case 1:
-                connectDB("select sum(salary) from payroll_service.employee_details where gender = 'F' group by gender");
-                break;
-            case 2:
-                connectDB("select avg(salary) from payroll_service.employee_details where gender = 'F' group by gender");
-                break;
-            case 3:
-                connectDB("select min(salary) from payroll_service.employee_details where gender = 'F' group by gender");
-                break;
-            case 4:
-                connectDB("select max(salary) from payroll_service.employee_details where gender = 'F' group by gender");
-                break;
-            case 5:
-                connectDB("select count(salary) from payroll_service.employee_details where gender = 'F' group by gender");
-                break;
-            default:
-                System.out.println("Invalid Input ! Please enter valid input !");
-        }
-    }
-
-    private static void connectDB(String string) throws Exception {
+    public static void main(String[] args) throws SQLException {
+        String jdbcURL = "jdbc:mysql://localhost:3307/payroll_service?useSSL=false";
+        String userName = "root";
+        String passWord = "1234";
         Connection connection = null;
         try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3307/payroll_service", "root", "1234");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loaded!");
+            System.out.println("Connecting to database :" + jdbcURL);
+            connection = DriverManager.getConnection(jdbcURL, userName, passWord);
+            System.out.println("Connection is successful!!!!" + connection);
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(string);
-            System.out.println("Connection is successful!!!!");
+            statement.execute("insert into payroll_service.employee_details values(4,'Sanket','M',250000,'2018-05-21')");
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM payroll_service.employee_details");
+            System.out.println("Update employee data from database :");
             while (resultSet.next()) {
-                System.out.println(resultSet.getInt(1));
+                System.out.println(resultSet.getInt(1)
+                        + " " + resultSet.getString(2)
+                        + " " + resultSet.getString(3)
+                        + " " + resultSet.getDouble(4)
+                        + " " + resultSet.getDate(5));
             }
         } catch (Exception exception) {
             System.out.println(exception.getMessage());
